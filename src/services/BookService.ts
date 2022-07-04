2201a46d40830562df59139625ec46b7a35faa8c
andre faria <andrefaria@upskill.pt>
2022-07-04 14:17:05 +0100

    Fix name

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -2,7 +2,7 @@ import IBookDTO from '../dtos/IBookDTO';
 import IBookService from './interfaces/IBookService';
 import { BookMapper } from '../mappers/BookMapper';
 import IBookRepository from '../repository/interfaces/IBookRepository';
-import { MongoBookRepository } from '../repository/mongo/MongoCategoryRepository';
+import { MongoBookRepository } from '../repository/mongo/MongoBookRepository';
 
 export class BookService implements IBookService {
     constructor(private bookRepository: IBookRepository = new MongoBookRepository()) {}
