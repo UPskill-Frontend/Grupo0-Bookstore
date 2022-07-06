a70f2692e72abdda0cade3138b5dcb3d341b9b78
andre faria <andrefaria@upskill.pt>
2022-07-06 14:06:19 +0100

    jwt

--- src/routes/BookRoute.ts
+++ src/routes/BookRoute.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 import { inject, injectable } from 'tsyringe';
 import { IBookController } from '../controllers/interfaces/IBookController';
+import { authenticateToken, login } from '../middlewares/login';
 
 @injectable()
 export class BookRoute {
     constructor(@inject('IBookController') private controller: IBookController) {}
 
     routes(app: Router) {
+        app.post('/api/login', login);
+        app.get('/api/validation', authenticateToken);
         app.post('/api/book', asyncHandler(this.controller.post));
         app.get('/api/book/publisher/:id', asyncHandler(this.controller.getByPublisher));
         app.get('/api/book/author/:nif', asyncHandler(this.controller.getByAuthor));
